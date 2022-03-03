5d25aac8b1883c271a726ea109fe113bb37a4295
Nicole Malinarich <nicom@DESKTOP-FURJVMF>
2022-03-03 12:58:50 +0100

    Revert "Proyecto MalinarichCastroNicole2122 comentado con javadoc"
    This reverts commit 4e450f45276c28b32f7e3250909a7e85441e3b4f.

--- src/jamon/JamonMalinarichCastroNicole2122.java
+++ src/jamon/JamonMalinarichCastroNicole2122.java
@@ -2,15 +2,8 @@
 package jamon;
 
 /**
- *Clase que reprenta a un <strong>jamón</strong>.
- * 
- * <p>Los objetos de esta clase permiten almacenar y gestionar la información sobre 
- * la categoria a la que pertenece el jamón, el precio, el stock asi como 
- * la compra venta de los jamones.</p> 
- * 
+ *
  * @author Nicole Malinarich
- * @version 1.0
- * @since 03/03/2022
  */
 public class JamonMalinarichCastroNicole2122 {
     
@@ -19,20 +12,10 @@ public class JamonMalinarichCastroNicole2122 {
     private int mesesCuracion;
     private int stock;
 
-    /**
-     * Constructor sin argumentos
-     */
-    
+    /* Constructor sin argumentos */
     public JamonMalinarichCastroNicole2122 ()
     {
     }
-    
-    /**
-     * Constructor con tres parámetros
-     * @param cat categoria del jamón
-     * @param precio precio del jamón
-     * @param stock cantidad de stock
-     */
     // Constructor con parámetro para iniciar todas las propiedades de la clase
     // jamon
     
@@ -42,43 +25,24 @@ public class JamonMalinarichCastroNicole2122 {
         this.precio=precio;
         this.stock=stock;
     }
-    /**
-     * Asigna una categoria al jamón
-     * @param cat categoria 
-     */
    // Método para asignar la categoría del jamón
     public void asignarCategoria(String cat)
     {
         setCategoria(cat);
     }
-    
-    /**
-     * Muestra la categoria a la que pertenece el jamón
-     * @return devuelve la categoria
-     */
     // Método que me devuelve la categoría del jamón
     public String obtenerCategoria()
     {
         return getCategoria();
     }
-    /**
-     * Muestra el stock de jamones disponibles en cada momento
-     * @return devuelve el stock del jamones disponibles.
-     */
 
     // Método que me devuelve el stock de jamones disponible en cada momento
      public int obtenerStock ()
     {
         return getStock();
     }
-     
-     /**
-      * Comprar jamones 
-      * @param cantidad cantidad de jamones que se van a comprar
-      * @throws Exception el número de jamones es negativo
-      */
 
-    /* Método para comprar jamones Modifica el stock.
+    /* Método para comprar coches. Modifica el stock.
      * Este método va a ser probado con Junit
      */
     public void comprar(int cantidad) throws Exception
@@ -87,12 +51,6 @@ public class JamonMalinarichCastroNicole2122 {
             throw new Exception("No se puede comprar un nº negativo de jamones");
         setStock(getStock() + cantidad);
     }
-    /**
-     * Vender jamones 
-     * @param cantidad cantidad de jamones para vender
-     * @param paisMalinarichCastroNicole2122 pais de origen
-     * @throws Exception cantidad de jamones es negativo, no hay suficientes jamones para vender.
-     */
 
     public void vender (int cantidad, String paisMalinarichCastroNicole2122) throws Exception
     {
@@ -104,64 +62,56 @@ public class JamonMalinarichCastroNicole2122 {
     }
 
     /**
-     * Muestra la categoria a la que pertenece el jamón
-     * @return devuelve la categoria
+     * @return the categoria
      */
     public String getCategoria() {
         return categoria;
     }
 
     /**
-     * Cambia la categoria a la que pertenece el jamón
-     * @param categoria nueva categoria 
+     * @param categoria the categoria to set
      */
     public void setCategoria(String categoria) {
         this.categoria = categoria;
     }
 
     /**
-     * Muestra el precio del jamón
-     * @return devuelve el precio
+     * @return the precio
      */
     public double getPrecio() {
         return precio;
     }
 
     /**
-     * Cambia el precio
-     * @param precio nuevo precio
+     * @param precio the precio to set
      */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
     /**
-     * Muestra los meses de curación 
-     * @return devuelve número de meses
+     * @return the mesesCuracion
      */
     public int getMesesCuracion() {
         return mesesCuracion;
     }
 
     /**
-     * Cambia el número de meses de curación
-     * @param mesesCuracion meses de curación 
+     * @param mesesCuracion the mesesCuracion to set
      */
     public void setMesesCuracion(int mesesCuracion) {
         this.mesesCuracion = mesesCuracion;
     }
 
     /**
-     * Muestra el stock de jamones disponibles
-     * @return devuelve el stock
+     * @return the stock
      */
     public int getStock() {
         return stock;
     }
 
     /**
-     * Cambia el número de stock de jamones
-     * @param stock número de jamones
+     * @param stock the stock to set
      */
     public void setStock(int stock) {
         this.stock = stock;
